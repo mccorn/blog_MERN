f2d8533a6fc223daa78392ed2e879493d795d3e5
Markeev Andrew <mccorn13@gmail.com>
2023-03-31 15:02:48 +0300

    finish registrarion methods

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = 3001;
 app.use(express.json());
 
 mongoose
-    .connect('mongodb+srv://mccorn13:markusha180593@cluster0.syiixd4.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://mccorn13:markusha180593@cluster0.syiixd4.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('DB OK'))
     .catch(err => console.log('DB ERROR!', err));
 
@@ -21,39 +21,44 @@ app.get('/', (req, res) => {
     res.send('He1 o');
 })
 
-// Просто для тестов
+
 app.post('/auth/login', (req, res) => {
-  console.log('post:: /auth/login', req.body);
+  try {
 
-  const token = jwt.sign({
-    email: req.body.email,
-    fullName: getUserNameByEmail(req.body.email),
-  }, getSecretKey())
+  } catch (err) {
 
-  res.json({
-    success: true,
-    token,
-  });
+  }
 })
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
+  try {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array())
-  } else {
-    const password = req.body.password;
-    const key = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, key);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    } else {
+      const password = req.body.password;
+      const key = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(password, key);
 
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: passwordHash,
-    });
+      const doc = new UserModel({
+        email: req.body.email,
+        fullName: req.body.fullName,
+        avatarUrl: req.body.avatarUrl,
+        passwordHash: hash,
+      });
 
-    res.json(getResponse('register'))
+      const user = await doc.save();
+
+      const token = jwt.sign({_id: user._id}, getSecretKey(), {expiresIn: "30d"});
+
+      let {passwordHash, ...userData} = user._doc
+
+      res.json({...userData, token});
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(getErrorResponse("error_register", err))
   }
 })
 
@@ -65,8 +70,12 @@ app.listen(PORT, (err) => {
   }
 })
 
-function getResponse(type, config = {success: true}) {
-  return Object.assign({ type }, config)
+function getResponse(type, config = {}) {
+  return Object.assign({ type, success: true }, config);
+}
+
+function getErrorResponse(type, config) {
+  return Object.assign({ type, success: false}, config);
 }
 
 function getSecretKey() {
